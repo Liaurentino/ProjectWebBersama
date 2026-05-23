dc62e0b20f009633a00ad32beccb0367a52c1b60
Liaurentino <liaurentinoneo25@gmail.com>
2026-05-23 22:49:16 +0700

    Changing Local Port

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
     res.send("Project Web Bersama - Backend");
 });
 
-const PORT = process.env.PORT || 5173;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server berjalan di port ${PORT}`);
